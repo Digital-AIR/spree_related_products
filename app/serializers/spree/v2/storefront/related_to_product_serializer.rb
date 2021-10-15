1522c54c6f449812e19e22b256995b84d0e8b188
adhik.mehta@digitalairtech.com <adhik.mehta@digitalairtech.com>
2021-10-15 15:37:11 +0530

    Fixed images in related product api

---
+++ app/serializers/spree/v2/storefront/related_to_product_serializer.rb
@@ -0,0 +1,18 @@
+module Spree
+  module V2
+    module Storefront
+      class RelatedToProductSerializer < BaseSerializer
+        set_type :related_to_product
+
+        attributes :name, :meta_description
+
+        has_many :images,
+                 object_method_name: :variant_images,
+                 id_method_name: :variant_image_ids,
+                 record_type: :image,
+                 serializer: :image
+
+      end
+    end
+  end
+end
